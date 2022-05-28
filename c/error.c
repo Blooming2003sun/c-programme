6b96527405c391407fddc4959b04f9f938b1b156
Blooming2003sun <rudreswar.pal.21@aot.edu.in>
2022-05-28 21:19:01 +0530

    staged

--- c/error.c
+++ c/error.c
@@ -1,15 +1,16 @@
-#include<stdio.h>
+#include <stdio.h>
 int main()
-{int a,b,i,s;
-printf("Enter starting number \n");
-scanf("%d",&a);
-printf("Enter closing number  \n");
-scanf("%d",&b);
-for(i=a ; i<=b;i++)
-{if(i%2!=0)
-printf(" %d \n ",i);
-}
+{
+    int a, b;
+    printf("Enter starting number \n");
+    scanf("%d", &a);
+    printf("Enter closing number  \n");
+    scanf("%d", &b);
+    for (int i = a; i <= b; i++)
+    {
+        if (i % 2 != 0)
+            printf("%d \n", i);
+    }
 
-
-return 0;	
+    return 0;
 }
--- c/new/table.c
+++ c/new/table.c
@@ -1,13 +1,17 @@
-#include<stdio.h>
-int main()
+// program to print table from 1 to 20
+#include <stdio.h>
+int main(void)
 
-{int x,n;
-    for(int x=1;x<=20;x++)
-{printf("This is  table of %d \n",x);
-for(int n=1;n<=10;n++)
+{
+    int x, n;
+    for (x = 1; x <= 20; x++)
     {
-        printf(" %d x %d = %d \n",x,n,x*n);}
-    printf("================\n");
-}
-return 0;	
+        printf("This is  table of %d \n", x);
+        for (n = 1; n <= 10; n++)
+        {
+            printf(" %d x %d = %d \n", x, n, x * n);
+        }
+        printf("================\n");
+    }
+    return 0;
 }
